16b7176d4140e1c1771198e6767c673b9aafe18f
Max <65973902+maxinlord@users.noreply.github.com>
2024-06-08 17:31:00 +0300

    fix

--- src_zoo_park/bot/handlers/inline_send_ref_link.py
+++ src_zoo_park/bot/handlers/inline_send_ref_link.py
@@ -19,7 +19,7 @@ from tools import get_text_message
 router = Router()
 
 
-@router.inline_query()
+@router.inline_query(F.query.in_(["Начни"]))
 async def share_referral_link(
     inline_query: InlineQuery,
     session: AsyncSession,
@@ -27,7 +27,7 @@ async def share_referral_link(
 ):
     link = await create_start_link(bot=inline_query.bot, payload=user.idpk)
     r = InlineQueryResultArticle(
-        id='ref',
+        id="ref",
         title=await get_text_message("title_inline_mess"),
         description=await get_text_message("description_inline_mess"),
         thumbnail_url="https://wepik.com/api/vector/9c37cdfd-1eed-4e23-a779-094960429bc4/preview",
@@ -37,7 +37,6 @@ async def share_referral_link(
         reply_markup=await ik_start_game(link=link),
     )
     await inline_query.answer(results=[r], cache_time=0)
-
 
 
 @router.chosen_inline_result(F.result_id == "ref")
