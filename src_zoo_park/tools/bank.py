33d262e8120ba1407b1f735208bc237c51f6fccf
Max <65973902+maxinlord@users.noreply.github.com>
2024-07-08 01:28:05 +0300

    update

--- src_zoo_park/tools/bank.py
+++ src_zoo_park/tools/bank.py
@@ -12,17 +12,17 @@ async def get_rate(session: AsyncSession, user: User):
     rate = await tools.get_value(
         session=session, value_name="RATE_RUB_USD", cache_=False
     )
-    if await tools.get_status_item(items=user.items, code_name_item="item_2"):
-        value = await session.scalar(
-            select(Item.value).where(Item.code_name == "item_2")
-        )
-        rate = rate * (1 - (value / 100))
     if d := await tools.get_values_from_item(items=user.items, code_name_item="item_6"):
         time_str = d.get("date_end")
         if not time_str:
             pass
         elif datetime.fromisoformat(time_str) >= datetime.now():
             rate = await tools.get_value(session=session, value_name="MIN_RATE_RUB_USD")
+    if await tools.get_status_item(items=user.items, code_name_item="item_2"):
+        value = await session.scalar(
+            select(Item.value).where(Item.code_name == "item_2")
+        )
+        rate = rate * (1 - (value / 100))
     return int(rate)
 
 
--- src_zoo_park/tools/base.py
+++ src_zoo_park/tools/base.py
@@ -11,16 +11,46 @@ def gen_key(length):
     )
 
 
+multipliers = {"k": 10**3, "к": 10**3, "m": 10**6, "м": 10**6, "b": 10**9, "б": 10**9}
+
+
+async def remove_punctuation_and_spaces(text: str) -> str:
+    # Регулярное выражение для замены всех знаков и пробелов на пустую строку
+    return re.sub(r"[^\w]", "", text)
+
+
 async def find_integers(text: str) -> int | None:
-    numbers = re.sub(r"[^\d\s]", " ", text)
-    integers = re.findall(r"\d+", numbers)
-    return int("".join(integers)) if integers else None
+    num: str = await remove_punctuation_and_spaces(text)
+
+    if not num:
+        return None
+    elif num.isdigit():
+        return int(num)
+    elif num.isalpha():
+        return None
+
+    match_num = re.search(r"\d+", num)
+
+    number = int(match_num.group())
+    num = num[match_num.end() :]
+    suffix = re.search(r"[^\W\d_]+", num)
+
+    suffix = (suffix.group()).lower()
+
+    if len(suffix) > 6:
+        return None
+
+    for s in suffix:
+        if s not in multipliers:
+            continue
+        number *= multipliers[s]
+    return int(number)
 
 
 async def fetch_and_parse_str_value(
     session: AsyncSession,
     value_name: str,
-    func_to_element = int,
+    func_to_element=int,
     sep: str = ",",
 ) -> list[any]:
     value_str = await tools.get_value(
